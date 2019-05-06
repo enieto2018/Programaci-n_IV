d9060772fb27964595558464ab2ceb1b78fb0c27
enieto2018 <e.nieto@utp.edu.co>
2019-05-05 23:37:54 -0500

    Clases con atributos

--- src/Test/AdministrarServiciosTest.java
+++ src/Test/AdministrarServiciosTest.java
@@ -1,10 +1,15 @@
 package Test;
 
 import Turnero.Servicios;
+import cap1.arrays.HashTable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
+
+import com.sun.org.apache.xml.internal.security.encryption.AgreementMethod;
+
 import Turnero.AdministrarServicios;
 import org.junit.Assert;   
 
@@ -13,7 +18,7 @@ import org.junit.Assert;
 	 @Test
 	    public void test_Servicios()
 	    {
-	    
+		
 	    }
 	
 }
--- src/Turnero/AdministrarServicios.java
+++ src/Turnero/AdministrarServicios.java
@@ -22,6 +22,5 @@ public class AdministrarServicios{
 	Servicios.add(newServicio);
 	
 	}
-	
 
 }
