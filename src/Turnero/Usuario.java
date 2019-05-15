ab8c517abc2cd5f18f32d07e3b999a1343aebf61
enieto2018 <e.nieto@utp.edu.co>
2019-05-15 01:09:45 -0500

    Clases con atributos

---
+++ src/Turnero/Usuario.java
@@ -0,0 +1,10 @@
+package Turnero;
+
+public class Usuario extends Persona {
+
+	public Usuario(int identificacion, String nombre) {
+		super(identificacion, nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+}
