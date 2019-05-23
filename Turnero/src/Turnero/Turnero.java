20a1846f7c05cfc9795390fb48040dabd4d0272b
estefania.nieto <estefania.nieto@datacenter.com.co>
2019-05-22 20:50:16 -0500

    Metodos asesor

--- Turnero/src/Turnero/Turnero.java
+++ Turnero/src/Turnero/Turnero.java
@@ -1,5 +1,15 @@
 package Turnero;
 
+import java.util.List;
+
 public class Turnero {
+	
+	public void enturnar(int identificacion,String nombre,Servicios servicio,Prioridad prioridad) {
+		
+		Turno turno =new Turno();
+		turno.setUsuario(new Usuario(identificacion, nombre));
+		
+		
+	}
 
 }
--- Turnero/src/Turnero/Turno.java
+++ Turnero/src/Turnero/Turno.java
@@ -1,6 +1,7 @@
 package Turnero;
 
 public class Turno {
+	
 	Usuario usuario;
 	Servicios servicio;
 	Prioridad prioridad;
