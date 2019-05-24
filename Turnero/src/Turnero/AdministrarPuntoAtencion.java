116dd5e8226d307737d770882cbe7634265b9d4f
estefania.nieto <estefania.nieto@datacenter.com.co>
2019-05-24 16:44:23 -0500

    Metodo enturnar

--- Turnero/src/Turnero/AdministrarPuntoAtencion.java
+++ Turnero/src/Turnero/AdministrarPuntoAtencion.java
@@ -5,24 +5,33 @@ import java.util.List;
 
 public class AdministrarPuntoAtencion 
 {
-	List<PuntoAtencion> PuntoAtencion;
+	List<PuntoAtencion> listaPuntoAtencion;
 	
+	public List<PuntoAtencion> getListaPuntoAtencion() {
+		return listaPuntoAtencion;
+	}
+
+
+
+	public void setListaPuntoAtencion(List<PuntoAtencion> listaPuntoAtencion) {
+		this.listaPuntoAtencion = listaPuntoAtencion;
+	}
+
+
+
 	public AdministrarPuntoAtencion() {
-				PuntoAtencion = new ArrayList<PuntoAtencion>();
+		listaPuntoAtencion = new ArrayList<PuntoAtencion>();
 	}
 
-	public List<PuntoAtencion> getPuntoAtencion() {
-		return PuntoAtencion;
-	}
 
-	public void setPuntoAtencion(List<PuntoAtencion> puntoAtencion) {
-		PuntoAtencion = puntoAtencion;
-	}
 	
 	public void AgregarPuntoAtencion(Asesor asesor,boolean disponible) {
-		PuntoAtencion newPuntoAte=new PuntoAtencion();
-		newPuntoAte.setAsesor(asesor);
-		PuntoAtencion.add(newPuntoAte);
+		if (listaPuntoAtencion.size()<=10) {
+			PuntoAtencion newPuntoAte=new PuntoAtencion();
+			newPuntoAte.setAsesor(asesor);
+			listaPuntoAtencion.add(newPuntoAte);
+		}
 	}
+	
 	
 }
