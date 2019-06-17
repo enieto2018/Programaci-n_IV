f0e0cdbd6ee44b6acf539a7476ca61b89e53e5bf
enieto2018 <e.nieto@utp.edu.co>
2019-06-17 17:32:59 -0500

    Vista

--- turnero_JPA_MVC/src/main/java/modelo/Prioridad.java
+++ turnero_JPA_MVC/src/main/java/modelo/Prioridad.java
@@ -1,6 +1,19 @@
 package modelo;
 
-public class Prioridad {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TABLA_PRIORIDAD")
+public class Prioridad implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "NOMBRE")
 	
 	private String nombre;
 
--- turnero_JPA_MVC/src/main/java/vista/VistaAsesor.java
+++ turnero_JPA_MVC/src/main/java/vista/VistaAsesor.java
@@ -7,9 +7,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class VistaAsesor extends JFrame {
+import controlador.AdministrarAsesor;
 
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VistaAsesor extends JFrame {
+	private AdministrarAsesor adminAsesor = new AdministrarAsesor();
 	private JPanel contentPane;
+	private JTextField txtIdentificacion;
+	private JTextField txtNombre;
+	private String nombre;
+	private String identificacion;
+	private boolean estado;
 
 	/**
 	 * Launch the application.
@@ -35,8 +49,52 @@ public class VistaAsesor extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtIdentificacion = new JTextField();
+		txtIdentificacion.setBounds(194, 36, 116, 22);
+		contentPane.add(txtIdentificacion);
+		txtIdentificacion.setColumns(10);
+		
+		JLabel lblIdentificacin = new JLabel("Identificaci\u00F3n");
+		lblIdentificacin.setBounds(32, 39, 116, 16);
+		contentPane.add(lblIdentificacin);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(32, 79, 56, 16);
+		contentPane.add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(194, 71, 116, 22);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JCheckBox chbEstado = new JCheckBox("Estado");
+		chbEstado.setBounds(32, 119, 113, 25);
+		contentPane.add(chbEstado);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				identificacion=txtIdentificacion.getText();
+				nombre=txtNombre.getText();
+				estado = chbEstado.isSelected();
+				
+				adminAsesor.agregarAsesor(nombre, identificacion, estado);
+			}
+			
+		});
+		btnGuardar.setBounds(176, 177, 97, 25);
+		contentPane.add(btnGuardar);
+		
+		JButton btnListar = new JButton("Listar");
+		btnListar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				adminAsesor.listarAsesor();
+			}
+		});
+		btnListar.setBounds(304, 177, 97, 25);
+		contentPane.add(btnListar);
 	}
-
 }
